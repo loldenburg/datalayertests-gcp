6bf59f578f724d94ee7db644455b87edc4247cb0
loldenburg <lukas.oldenburg@dim28.ch>
2022-09-19 10:36:20 +0200

    cross-linking to other repo

--- datalayer_tests/log_datalayer_error.py
+++ datalayer_tests/log_datalayer_error.py
@@ -116,7 +116,7 @@ def run_script(payload=None):
 
     sql_write = f"INSERT INTO `{table_id}` " \
                 f"(event_id, event_name, error_types, error_vars, logged_at, url_full, user_id, tealium_profile) VALUES " \
-                f"('{log_id}', '{event_name}', '{' '.join(error_types)}', '{' '.join(error_types)}', '{logged_at}', '{url_full}', '{user_id}', '{tealium_profile}')"
+                f"('{log_id}', '{event_name}', '{';'.join(error_types)}', '{';'.join(error_types)}', '{logged_at}', '{url_full}', '{user_id}', '{tealium_profile}')"
     query_job_write = client.query(sql_write)
     query_job_write.result()
     log().info(f"Wrote to BigQuery table {table_id}: {sql_write}")
